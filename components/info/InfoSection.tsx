f730bdbdf79febb8c0b6afbb9e232d8ef43e64aa
Kyle Stewart <kstew1017@outlook.com>
2023-09-12 12:30:33 -0500

    image fixes

--- components/info/InfoSection.tsx
+++ components/info/InfoSection.tsx
@@ -9,7 +9,6 @@ const InfoSection = () => {
         <section className="bg-tan-100 z-20 relative">
             <div className="grid gap-y-[50px] grid-rows-2 w-[90%] lg:max-w-[1250px] items-center mx-auto pb-[50px] pt-[50px] lg:pb-[0px] lg:pt-[0px]">
                 <AboutUsRow
-                    imageSrc="/temp-hero.jpg"
                     imageAlt="temp"
                     title="About Us"
                 />
--- components/info/aboutUs/AboutUsRow.tsx
+++ components/info/aboutUs/AboutUsRow.tsx
@@ -8,17 +8,16 @@ import { AboutUsCard } from "./AboutUsCard";
 
 
 interface AboutUsCardProps {
-  imageSrc: string;
   imageAlt: string;
   title: string;
 };
 
-const AboutUsRow: FC<AboutUsCardProps> = ({ imageSrc, imageAlt, title }) => {
+const AboutUsRow: FC<AboutUsCardProps> = ({ imageAlt, title }) => {
     return (
         <Reveal hiddenVariant="hiddenY" visibleVariant="visibleY" delay={0.2 + Math.random() * 0.4}>
             <div className="hidden lg:grid grid-cols-10 items-center lg:mt-[30px]">
                 <AboutUsImage
-                    imageSrc={imageSrc}
+                    imageSrc="/temp-hero.jpg"
                     imageAlt={imageAlt}
                 />
                 <AboutUsCard title={title} />
@@ -26,7 +25,7 @@ const AboutUsRow: FC<AboutUsCardProps> = ({ imageSrc, imageAlt, title }) => {
 
             <div className="lg:hidden grid grid-rows-auto grid-cols-10 items-center">
                 <AboutUsImage
-                    imageSrc={imageSrc}
+                    imageSrc="/temp-hero.jpg"
                     imageAlt={imageAlt}
                 />
                 <div className="col-span-full">
